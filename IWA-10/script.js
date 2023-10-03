77fabb2a67ffbf2109b8def9f6425844b0040ae2
Josh-Twomey <140522624+Josh-Twomey@users.noreply.github.com>
2023-10-03 17:52:39 +0200

    Fixed IWA10 code

--- IWA-10/script.js
+++ IWA-10/script.js
@@ -77,36 +77,41 @@ console.log("Date change:",holidays[christmas].date != copied.date ? copied.date
 }
 
 const firstHolidayTimestamp = Math.min(
-  holidays[0].date.getTime,
-  holidays[1].date.getTime,
-  holidays[2].date.getTime,
-  holidays[3].date.getTime,
-  holidays[4].date.getTime,
-  holidays[5].date.getTime,
-  holidays[6].date.getTime,
-  holidays[7].date.getTime,
-  holidays[8].date.getTime
+  new Date (holidays[0].date).getTime(),
+  holidays[1].date.getTime(),
+  holidays[2].date.getTime(),
+  holidays[3].date.getTime(),
+  holidays[4].date.getTime(),
+  holidays[5].date.getTime(),
+  holidays[6].date.getTime(),
+  holidays[7].date.getTime(),
+  holidays[8].date.getTime()
 );
+
+
 
 const lastHolidayTimestamp = Math.max(
-  holidays[0].date.getTime,
-  holidays[1].date.getTime,
-  holidays[2].date.getTime,
-  holidays[3].date.getTime,
-  holidays[4].date.getTime,
-  holidays[5].date.getTime,
-  holidays[6].date.getTime,
-  holidays[7].date.getTime,
-  holidays[8].date.getTime
+  new Date (holidays[0].date).getTime(),
+  holidays[1].date.getTime(),
+  holidays[2].date.getTime(),
+  holidays[3].date.getTime(),
+  holidays[4].date.getTime(),
+  holidays[5].date.getTime(),
+  holidays[6].date.getTime(),
+  holidays[7].date.getTime(),
+  holidays[8].date.getTime()
 );
 
-const firstDay = firstHolidayTimestamp.getDate;
-const firstMonth = firstHolidayTimestamp.getMonth;
-const lastDay = lastHolidayTimestamp.getDate;
-const lastMonth = lastHolidayTimestamp.getMonth;
 
-console.log("{firstDay}/{firstMonth}/{currentYear}");
-console.log("{lastDay}/{lastMonth}/{currentYear}");
+const firstDay = new Date (firstHolidayTimestamp).getDate();
+const firstMonth = (new Date (firstHolidayTimestamp).getMonth())+1;
+const lastDay = new Date (lastHolidayTimestamp).getDate();
+const lastMonth = (new Date (lastHolidayTimestamp).getMonth())+1;
 
-const randomHoliday = (Math.random * 100);
-console.log(holidays[randomHoliday].date);
+console.log(`${String(firstDay).padStart(2,0)}/${String(firstMonth).padStart(2,0)}/${currentYear}`);
+console.log(`${lastDay}/${lastMonth}/${currentYear}`);
+
+const randomHoliday = holidays[Math.round(Math.random() * 8)];
+const randomDay = randomHoliday.date.getDate();
+const randomMonth = randomHoliday.date.getMonth() + 1;
+console.log(`${String(randomDay).padStart(2, 0)}/${String(randomMonth).padStart(2,0)}/${currentYear}`);
